e7a295e28e99ea40da2f7549638911083ef7fbe7
VelascoCristian <81784222+VelascoCristian@users.noreply.github.com>
2023-02-20 21:45:29 -0300

    Add another way

--- device_detection.js
+++ device_detection.js
@@ -56,3 +56,49 @@ export default function userDeviceInfo(id) {
         $id.innerHTML += `<p><mark> You can view this only in windows </mark></p>`;
     }
 }
+
+/*export default function userDeviceInfo(id) {
+    const $id = document.getElementById(id),
+      platform = navigator.platform,
+      isMobile = {
+        android: () => platform.match(/android/i),
+        ios: () => platform.match(/iphone|ipad|ipod/i),
+        any: function() {
+          return this.android() || this.ios();
+        },
+      },
+      isDesktop = {
+        linux: () => platform.match(/linux/i),
+        mac: () => platform.match(/mac/i),
+        windows: () => platform.match(/win/i),
+        any: function() {
+          return this.linux() || this.mac() || this.windows();
+        },
+      },
+      isBrowser = {
+        chrome: () => navigator.userAgent.match(/chrome/i),
+        safari: () => navigator.userAgent.match(/safari/i),
+        firefox: () => navigator.userAgent.match(/firefox/i),
+        opera: () => navigator.userAgent.match(/opera|opera mini/i),
+        ie: () => navigator.userAgent.match(/msie|iemobile/i),
+        edge: () => navigator.userAgent.match(/edge/i),
+        any: function() {
+          return this.ie() || this.opera() || this.chrome() || this.safari() || this.firefox() || this.edge();
+        },
+      };
+  
+    $id.innerHTML = `
+      <ul>
+        <li>Platform: <b>${isMobile.any() ? isMobile.any() : isDesktop.any()}</b></li>
+        <li>Navigator: <b>${isBrowser.any()}</b></li>
+      </ul>
+    `;
+  
+    if (isBrowser.chrome()) {
+      $id.innerHTML += `<p><mark>You can view this only in Chrome.</mark></p>`;
+    }
+  
+    if (isDesktop.windows()) {
+      $id.innerHTML += `<p><mark>You can view this only in Windows.</mark></p>`;
+    }
+  }*/
