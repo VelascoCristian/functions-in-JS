c9f8de6b48b16208961bfd69e94c3be00bd7a248
VelascoCristian <81784222+VelascoCristian@users.noreply.github.com>
2023-03-07 22:18:25 -0300

    Add script and API fetch

--- web con ajax/script/script.js
+++ web con ajax/script/script.js
@@ -1,31 +1,91 @@
-/*const goToAjax = (id, web) => {
-  document.addEventListener("click", (e) => {
-    const $link = document.getElementById(id);
+const d = document,
+  $main = d.querySelector("main");
+let count = 0;
 
-    if (e.target.matches(id)) {
-      e.preventDefault();
-      alert("SISI");
-    }
-  });
+const getHTML = (options) => {
+  let { url, cbSucces, error } = options;
+
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.text();
+      } else {
+        throw new Error(`Error ${response.status}: ${response.statusText}`);
+      }
+    })
+    .then((html) => {
+      cbSucces(html);
+    })
+    .catch((err) => {
+      error(`Error: ${err.message}`);
+    });
 };
 
 document.addEventListener("DOMContentLoaded", (e) => {
-  goToAjax("services-link", "weasdaaa");
-});*/
-
-document
-  .getElementById("services-link")
-  .addEventListener("click", function (e) {
-    e.preventDefault(); // Previene el comportamiento predeterminado del enlace
-
-    // Realiza una solicitud AJAX con Axios
-    axios
-      .get("services.html")
-      .then(function (response) {
-        // Agrega el contenido obtenido a la página actual
-        document.documentElement.innerHTML = response.data;
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+  getHTML({
+    url: "assets/home.html",
+    cbSucces: (html) => ($main.innerHTML = html),
+    error: (err) => ($main.innerHTML = `<h1>${err}</h1>`),
   });
+
+  // For page with sliders
+  d.addEventListener("click", (e) => {
+    if (e.target.matches(".nav a")) {
+      e.preventDefault();
+      getHTML({
+        url: e.target.href,
+        cbSucces: (html) => ($main.innerHTML = html),
+        error: (err) => ($main.innerHTML = `<h1>${err}</h1>`),
+      });
+    }
+
+    if (
+      e.target.matches(".slider-btns .prev") ||
+      e.target.matches(".slider-btns .next")
+    ) {
+      const $nextBtn = d.querySelector(".slider-btns .next"),
+        $prevBtn = d.querySelector(".slider-btns .prev"),
+        $slides = d.querySelectorAll(".slider-slide");
+
+      d.addEventListener("click", (e) => {
+        if (e.target === $prevBtn) {
+          e.preventDefault();
+          $slides[count].classList.remove("active");
+          count--;
+
+          if (count < 0) count = $slides.length - 1;
+
+          $slides[count].classList.add("active");
+        }
+
+        if (e.target === $nextBtn) {
+          e.preventDefault();
+          $slides[count].classList.remove("active");
+          count++;
+
+          if (count >= $slides.length) count = 0;
+
+          $slides[count].classList.add("active");
+        }
+      });
+
+      //As long as there is no interaction, the slider will continue to move forward
+      let sliderInterval;
+      function nextSlide() {
+        $slides[count].classList.remove("active");
+        count++;
+
+        if (count >= $slides.length) count = 0;
+
+        $slides[count].classList.add("active");
+      }
+
+      sliderInterval = setInterval(nextSlide, 5500);
+
+      $nextBtn.addEventListener("click", () => {
+        clearInterval(sliderInterval);
+        nextSlide();
+      });
+    }
+  });
+});
